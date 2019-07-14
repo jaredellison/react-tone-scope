b45c7bf144b391c283f3143feea0c29a930ae913
Jared Ellison <jaredellison@gmail.com>
2019-07-14 10:51:13 -0400

    Add tone modules to BirdVoice

--- client/dist/bird.js
+++ client/dist/bird.js
@@ -36,13 +36,12 @@ const dbScale = n => {
   return Math.log10(n) * 20;
 };
 
-
 // Modules:
 //  Env - 5
 //  Osc - 3
 //  VCA - 5 +?
 
-let masterVolume = new Tone.Volume;
+let masterVolume = new Tone.Volume();
 
 let osc = new Tone.Oscillator();
 osc.chain(masterVolume, Tone.master);
@@ -65,12 +64,9 @@ volumeSlider.addEventListener('input', e => {
   masterVolume.volume.value = dbScale(e.target.value);
 });
 
-
 class Bird {
   constructor() {
-    this.modules = {
-
-    }
+    this.modules = {};
   }
 }
 
@@ -85,13 +81,13 @@ class FreqMod {
     this.attack = attack || 0;
     this.decay = decay || 0;
 
-    // Statc Params
+    // Static Params
     this.attackDecayScale = 900;
     this.modulationScale = 3000;
 
     // Tone modules
-    this.envelope = new Tone.Envelope;
-    this.vca = new Tone.Volume;
+    this.envelope = new Tone.Envelope();
+    this.vca = new Tone.Volume();
     this.multiply = new Tone.Multiply(this.modulationScale);
 
     // Connect modules
@@ -100,7 +96,10 @@ class FreqMod {
   }
 
   trigger() {
-    this.node.triggerAttackRelease(this.attack, this.decay);
+    this.node.triggerAttackRelease(
+      this.attack * this.attackDecayScale,
+      this.decay * this.attackDecayScale
+    );
   }
 
   set modulation(value) {
@@ -116,24 +115,71 @@ class AmpMod {
     this.attack = attack || 0;
     this.decay = decay || 0;
 
-    // Statc Params
+    // Static Params
     this.attackDecayScale = 900;
 
     // Tone modules
-    this.envelope = new Tone.Envelope;
-    this.vca = new Tone.Volume;
+    this.envelope = new Tone.Envelope();
+    this.vca = new Tone.Volume();
 
     // Connect modules
     this.envelope.chain(this.vca, this.multiply);
+
     this.output = this.vca;
   }
 
   trigger() {
-    this.node.triggerAttackRelease(this.attack, this.decay);
+    this.node.triggerAttackRelease(
+      this.attack * this.attackDecayScale,
+      this.decay * this.attackDecayScale
+    );
   }
 
   set modulation(value) {
     this.modulation = value;
     this.vca.value = value;
+  }
+}
+
+class BirdVoice {
+  constructor(attack, decay) {
+    /////////////////////
+    //  Tone modules
+    // Frequency Modulation
+    this.freqModLfo = new Tone.Oscillator();
+    this.freqModVca1 = new Tone.Volume();
+    this.freqModOffset = new Tone.Add(1);
+    this.freqModVca2 = new Tone.Volume();
+
+    // Amplitude Modulation
+    this.ampModLfo = new Tone.Oscillator();
+    this.ampModVca = new Tone.Volume();
+    this.ampModOffset = new Tone.Add(-1);
+
+    // Main Voice
+    this.osc = new Tone.Oscillator();
+    this.env = new Tone.Envelope();
+    this.vca = new Tone.Volume();
+
+    // Expose Inputs
+    this.freqModInitIn = this.freqModVca2.volume;
+    this.freqModAmpIn = this.freqModVca1.volume;
+    this.freqModFreqIn = this.freqModLfo.frequency;
+
+    this.ampModAmpIn = this.ampModVca.volume;
+    this.ampModFreqIn = this.ampModLfo.frequency;
+
+    // Connect modules
+
+
+    this.output = this.vca;
+  }
+
+  trigger() {
+    this.env.triggerAttackRelease(this.attack, this.decay);
+  }
+
+  set frequency(value) {
+    this.frequency = value;
   }
 }
