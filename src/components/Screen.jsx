2c44f214633bd145333240e6141acdcd2b83e433
Jared Ellison <jaredellison@gmail.com>
2021-03-06 16:24:34 -0500

    Transform Screen to functional component

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -3,85 +3,76 @@ import React from 'react';
 import Divisions from './Divisions.jsx';
 import { scaleCoordinate } from '../utils/utils.js';
 
-class Screen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      height: 280,
-      width: 350
-    };
-  }
+const SCREEN_HEIGHT = 280;
+const SCREEN_WIDTH = 350;
 
-  render() {
-    const { width, height } = this.state;
-    const {
-      divsV,
-      divsH,
-      samples,
-      verticalScale,
-      renderTiggerLine,
-      triggerValue
-    } = this.props;
+const Screen = ({
+  divsV,
+  divsH,
+  samples,
+  verticalScale,
+  renderTiggerLine,
+  triggerValue,
+}) => {
+  const traceString = samples.reduce((a, v, i) => {
+    const x = scaleCoordinate(i, 0, samples.length, 0, SCREEN_WIDTH);
+    const y = scaleCoordinate(-1 * v, -1, 1, 0, SCREEN_HEIGHT, 0.25 / verticalScale);
 
-    const traceString = samples.reduce((a, v, i) => {
-      const x = scaleCoordinate(i, 0, samples.length, 0, width);
-      const y = scaleCoordinate(-1 * v, -1, 1, 0, height, 0.25 / verticalScale);
-
-      // Set starting position
-      if (i === 0) {
-        return a + `M ${x}, ${y} `;
-      } else {
-        return a + `L ${x}, ${y} `;
-      }
-    }, '');
-
-    let triggerLine = null;
-    if (renderTiggerLine) {
-      const triggerY = scaleCoordinate(-1 * triggerValue, -1, 1, 0, height, 0.25 / verticalScale);
-      triggerLine = (
-        <line
-          x1="0"
-          y1={triggerY}
-          x2={width}
-          y2={triggerY}
-          stroke={'red'}
-        />
-      );
+    // Set starting position
+    if (i === 0) {
+      return a + `M ${x}, ${y} `;
+    } else {
+      return a + `L ${x}, ${y} `;
     }
+  }, '');
 
-    return (
-      <svg
-        viewBox={`0 0 ${width} ${height}`}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        stroke="black"
-        fill="white"
-      >
-        <rect width={width} height={height} rx="5" />
-        <Divisions
-          orientation={'vertical'}
-          total={divsV}
-          width={width}
-          height={height}
-        />
-        <Divisions
-          orientation={'horizontal'}
-          total={divsH}
-          width={width}
-          height={height}
-        />
-        <path
-          d={traceString}
-          stroke="blue"
-          strokeWidth="2"
-          strokeLinecap="round"
-          fill="transparent"
-        />
-        {triggerLine}
-      </svg>
+  let triggerLine = null;
+  if (renderTiggerLine) {
+    const triggerY = scaleCoordinate(
+      -1 * triggerValue,
+      -1,
+      1,
+      0,
+      SCREEN_HEIGHT,
+      0.25 / verticalScale
+    );
+    triggerLine = (
+      <line x1="0" y1={triggerY} x2={SCREEN_WIDTH} y2={triggerY} stroke={'red'} />
     );
   }
-}
+
+  return (
+    <svg
+      viewBox={`0 0 ${SCREEN_WIDTH} ${SCREEN_HEIGHT}`}
+      width={SCREEN_WIDTH}
+      height={SCREEN_HEIGHT}
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="black"
+      fill="white"
+    >
+      <rect width={SCREEN_WIDTH} height={SCREEN_HEIGHT} rx="5" />
+      <Divisions
+        orientation={'vertical'}
+        total={divsV}
+        width={SCREEN_WIDTH}
+        height={SCREEN_HEIGHT}
+      />
+      <Divisions
+        orientation={'horizontal'}
+        total={divsH}
+        width={SCREEN_WIDTH}
+        height={SCREEN_HEIGHT}
+      />
+      <path
+        d={traceString}
+        stroke="blue"
+        strokeWidth="2"
+        strokeLinecap="round"
+        fill="transparent"
+      />
+      {triggerLine}
+    </svg>
+  );
+};
 
 export default Screen;
