34aad83a9b52a14ac9d7cc4431a4cde123d1588e
Jared Ellison <jaredellison@gmail.com>
2021-03-06 13:13:37 -0500

    Remove unused babel config

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-const isTest = String(process.env.NODE_ENV) === 'test'
-
-module.exports = {
-  presets: [['@babel/env', {modules: isTest ? 'commonjs' : false}], '@babel/react'],
-  plugins: [
-    'syntax-dynamic-import',
-    isTest ? 'dynamic-import-node' : null,
-  ].filter(Boolean),
-}
