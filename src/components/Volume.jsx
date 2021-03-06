15e026a0823a8fa30843bf5d18cff1952e650114
Jared Ellison <jaredellison@gmail.com>
2021-03-06 15:26:55 -0500

    Tidy up volume component

--- src/components/Volume.jsx
+++ src/components/Volume.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
 
-const Volume = props => {
-  const { mute, value, setMute, setValue } = props;
-
-  return (
-    <div className="volume-container">
-      <label htmlFor="mute" className="control-label">
-        Mute
-      </label>
-      <input
-        type="checkbox"
-        id="mute"
-        name="mute"
-        onChange={() => setMute(!mute)}
-        checked={mute}
-      />
-      <label htmlFor="volume-control" className="control-label">
-        Volume Control
-      </label>
-      <input
-        type="range"
-        id="volume-control"
-        name="volume-control"
-        min={0}
-        max={1}
-        step={0.01}
-        onChange={e => {
-          const value = Number(e.target.value);
-          setValue(value);
-        }}
-        defaultValue={value}
-      />
-    </div>
-  );
-};
+const Volume = ({ mute, value, setMute, setValue }) => (
+  <div className="volume-container">
+    <label htmlFor="mute" className="control-label">
+      Mute
+    </label>
+    <input type="checkbox" id="mute" name="mute" onChange={() => setMute(!mute)} checked={mute} />
+    <label htmlFor="volume-control" className="control-label">
+      Volume Control
+    </label>
+    <input
+      type="range"
+      id="volume-control"
+      name="volume-control"
+      min={0}
+      max={1}
+      step={0.01}
+      onChange={(e) => {
+        const value = Number(e.target.value);
+        setValue(value);
+      }}
+      defaultValue={value}
+    />
+  </div>
+);
 
 export default Volume;
